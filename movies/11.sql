a09b11a195f639b44216dbe6e69dd321680d6b7f
Jonas Savickas <jonsavickas@gmail.com>
2025-03-10 20:07:20 +0200

    pradzia

---
+++ movies/11.sql
@@ -0,0 +1,3 @@
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (
+    SELECT id FROM people WHERE name = 'Chadwick Boseman')
+) ORDER BY (SELECT rating FROM ratings WHERE ratings.movie_id = movies.id) DESC LIMIT 5;
